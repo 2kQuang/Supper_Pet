79f47c8a18ea7bb4226698b509df10c72d9aeb8f
2kQuang <tvquang.19i@gmail.com>
2022-11-14 23:04:23 +0700

    face 1

--- resources/views/admin/pet/index.blade.php
+++ resources/views/admin/pet/index.blade.php
@@ -2,6 +2,34 @@
 @section('content_admin')
   @extends('layouts._sidebar')
   @section('content')
-  
+    <div class="header-content">
+      <div class="btn-add"><a href="">Thêm</a></div>
+    </div>
+    <div class="body-content">
+      <table class="table" >
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th colspan="2" scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          @forelse($Pets as $k => $v)
+            <tr>
+              <td>{{ $v['pet'] }}</td>
+              <td><a href="">Update</a></td>
+              <td><a href="">Delete</a></td>
+            </tr>
+          @empty
+            <tr>
+              <td colspan="3">No data <a href="">Thêm</a></td>
+            </tr>
+          @endforelse
+        </tbody>
+      </table>
+    </div>
+    <div class="footer-content">
+      <div class="btn-add"><a href="">Thêm</a></div>
+    </div>
   @endsection
 @endsection
